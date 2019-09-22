973c04b60e02d17cece31ce6b8b49f8e6aba50be
Jonconan <jun.miyahara.1020@gmail.com>
2019-09-23 01:27:28 +0900

    [add] seed追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_22_155904) do
-
-  create_table "areas", force: :cascade do |t|
-    t.integer "prefecture_id", null: false
-    t.string "name", null: false
-    t.boolean "del_flg", default: false, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2019_09_22_155827) do
 
   create_table "prefectures", force: :cascade do |t|
     t.integer "region_id", null: false
