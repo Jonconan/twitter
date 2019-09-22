fb78d192a814e02d1612f24ae9c1b0296368fe85
Jonconan <jun.miyahara.1020@gmail.com>
2019-09-23 01:07:03 +0900

    [add] migration＆seeds追加

---
+++ db/migrate/20190922155904_create_areas.rb
@@ -0,0 +1,11 @@
+class CreateAreas < ActiveRecord::Migration[5.2]
+  def change
+    create_table :areas do |t|
+      t.int :prefecture_id, null: false
+      t.string :name, null: false
+      t.boolean :del_flg, null: false, default: false
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Region.create(:name => '北海道')
+Region.create(:name => '東北地方')
+Region.create(:name => '関東地方')
+Region.create(:name => '中部地方')
+Region.create(:name => '近畿地方')
+Region.create(:name => '中国地方')
+Region.create(:name => '四国地方')
+Region.create(:name => '九州・沖縄地方')
